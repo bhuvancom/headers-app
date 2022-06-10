98d7a80d3915789b620cbd221c181cb39cc7acd2
Bhuvaneshvar nath Srivastava <bhuvaneshvar8@gmail.com>
2022-06-10 23:31:33 +0530

    Update cf_script.js

--- cf_script.js
+++ cf_script.js
@@ -1,12 +1,25 @@
 addEventListener("fetch", event => {
-event.respondWith(doRedirects(event.request))
+  event.respondWith(doRedirects(event.request));
 })
-const newLocationHost = "developers.cloudflare.com";
+
+const newLocationHost = "developers.cloudflare.com/workers/about";
 async function doRedirects(request) {
-let reqUA = req.headers.get('user-agent')
-if (reqUA.matches('curl').true) {
-let newLocation = "https://"+newLocationHost
-return Response.redirect(newLocation, 302)
-}
-return fetch(request);
+ const { headers: { cookie } } = req;
+    if (cookie) {
+        const values = cookie.split(';').reduce((res, item) => {
+            const data = item.trim().split('=');
+            return { ...res, [data[0]]: data[1] };
+        }, {});
+        console.log(values);
+        if(values['cf-noredir']){
+           return fetch(request);
+        }
+    }
+  
+  let reqUA = req.headers.get('user-agent')
+  if (reqUA.matches('curl').true) {
+      let newLocation = "https://"+newLocationHost
+      return Response.redirect(newLocation, 302)
+  }
+  return fetch(request);
 }
