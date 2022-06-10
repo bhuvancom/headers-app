e552e11b285330b89bbb603315437d6ae2aec901
Bhuvaneshvar nath Srivastava <bhuvaneshvar8@gmail.com>
2022-06-10 22:07:52 +0530

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
+app.get('/headers', function(req, res){
     const headers = req.headers;
     res.send(header);
 });
